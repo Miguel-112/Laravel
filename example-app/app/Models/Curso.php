cc81d80d542ff7cbdbd6a8fdb43e31afcd603f5a
Miguel-112 <71301712+Miguel-112@users.noreply.github.com>
2022-10-16 19:40:24 -0500

    Trabajando con los seeder y los factorys en laravel
    Trabajando con los seeder y los factorys

---
+++ example-app/app/Models/Curso.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Curso extends Model
+{
+    use HasFactory;
+
+    //por defecto administra la tabla curso, pero si se quiere administrar otra tabla se le asigna el siguiente comando
+
+    //protected $table = "users";
+}
+
+
--- example-app/database/seeders/DatabaseSeeder.php
+++ example-app/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Curso;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-    }
+
+        Curso::factory(50)->create(); //lo llena con 50 elementos
+           
+            
+        }
 }
